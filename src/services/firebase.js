5106419dd61ccec71d2dabbca320d7a3a4fc79e0
Your Name <chasov90@gmail.com>
2025-12-19 15:47:27 +0200

    Move Firebase config to environment variables for security

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhIZaEAJZe-q7R3wNkuzNeEIZQeNnRtI4",
-  authDomain: "nanny-709dc.firebaseapp.com",
-  databaseURL:
-    "https://nanny-709dc-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "nanny-709dc",
-  storageBucket: "nanny-709dc.firebasestorage.app",
-  messagingSenderId: "221473095034",
-  appId: "1:221473095034:web:65c72fe726aee1955d6ac3",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
